9bd2348428a1c83710762860885660c7ca0f792e
John Slota <jslota17@comcast.net>
2020-04-30 23:08:26 -0500

    Switch statements

--- server.js
+++ server.js
@@ -30,4 +30,37 @@ function runProgram() {
 
         ])
 
-        
+        .then(function (res) {
+            switch (res.prompt) {
+                case "Add a department":
+                    console.log("You are about to add department.")
+                    addDepartment();
+                    break;
+                case "Add a role":
+                    console.log("You are about to add a role.")
+                    addRole();
+                    break;
+                case "Add an employee":
+                    console.log("You are about to add an employee.")
+                    addEmployee();
+                    break;
+                case "View departments":
+                    console.log("You are about to view all departments.")
+                    viewDepartments();
+                    break;
+                case "View roles":
+                    console.log("You are about to view all roles.")
+                    viewRoles()
+                    break;
+                case "View employees":
+                    console.log("You are about to view all employees")
+                    viewEmployees();
+                    break;
+                case "Update an employee role":
+                    console.log("You are about to update an employee role.")
+                    updateEmployeeRole();
+                    break;
+            }
+            console.log(response.addViewUpdate);
+        });
+}
